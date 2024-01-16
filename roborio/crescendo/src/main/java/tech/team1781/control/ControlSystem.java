29f4f904a26f26c33bfa598192229b3eb2c8491c
VincentD <vdizon@cps.edu>
2024-01-16 14:19:31 -0600

    max radians per second

--- roborio/crescendo/src/main/java/tech/team1781/control/ControlSystem.java
+++ roborio/crescendo/src/main/java/tech/team1781/control/ControlSystem.java
@@ -51,7 +51,7 @@ public class ControlSystem {
         mDriveSystem.drawWithMaxVelo(
             mXDriveLimiter.calculate(translation.y), 
             mYDriveLimiter.calculate(translation.x), 
-            mRotDriveLimiter.calculate(rotation.x));
+            mRotDriveLimiter.calculate(rotation.y));
     }
 
     public void zeroNavX() {
