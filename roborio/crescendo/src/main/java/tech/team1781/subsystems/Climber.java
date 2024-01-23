90eb2c14e4372a20d4ffbe29f31efd3de99c0b3d
Joaquin Herrera <jherrera132@cps.edu>
2024-01-23 17:16:13 -0600

    Worked on getToState, matchesDesiredState
    Added basic code to the aforementioned methods

--- roborio/crescendo/src/main/java/tech/team1781/subsystems/Climber.java
+++ roborio/crescendo/src/main/java/tech/team1781/subsystems/Climber.java
@@ -1,12 +1,27 @@
 package tech.team1781.subsystems;
 
-public class Climber extends Subsystem{
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkLimitSwitch.Type;
+import com.revrobotics.SparkLimitSwitch;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Climber extends Subsystem {
+
+    private CANSparkMax motor = new CANSparkMax(0, MotorType.kBrushless);
+    private RelativeEncoder encoder = motor.getEncoder();
+    private ClimberState mClimberState = ClimberState.IDLE;
+    private ClimberState mDesiredClimberState = ClimberState.IDLE;
+    private SparkLimitSwitch mDownLimitSwitch = motor.getReverseLimitSwitch(Type.kNormallyOpen);
+    private SparkLimitSwitch mUpLimitSwitch = motor.getForwardLimitSwitch(Type.kNormallyOpen);
 
     public Climber() {
         super("Climber", ClimberState.IDLE);
     }
 
-    public enum ClimberState implements Subsystem.SubsystemState{
+    public enum ClimberState implements Subsystem.SubsystemState {
         IDLE, EXTEND, RETRACT
     }
 
@@ -20,12 +35,37 @@ public class Climber extends Subsystem{
 
     @Override
     public void getToState() {
+        switch (mDesiredClimberState) {
+            case IDLE:
+                    motor.set(0);
+                break;
+
+            case EXTEND:
+                if (encoder.getPosition() < 100) {
+                    motor.set(0.5);
+                }
+                break;
+
+            case RETRACT:
+                if (encoder.getPosition() > 0) {
+                    motor.set(-0.5);
+                }
+                break;
+        }
     }
 
     @Override
     public boolean matchesDesiredState() {
-        return false;
-    }
+        switch((ClimberState) getState()) {
+            case IDLE:
+                return motor.get() == 0; 
+            case EXTEND:
+                return motor.get() == .5; 
+            case RETRACT:
+                return motor.get() == -.5;
+
+    }return false;
+}
 
     @Override
     public void autonomousPeriodic() {
@@ -33,6 +73,7 @@ public class Climber extends Subsystem{
 
     @Override
     public void teleopPeriodic() {
+
     }
-    
+
 }
