d0e7818b033ccdc24804c90cdc4e727b8d852cf2
Vincent Dizon <vdizon@cps.edu>
2024-01-27 12:20:04 -0600

    .

--- roborio/crescendo/src/main/java/tech/team1781/subsystems/Arm.java
+++ roborio/crescendo/src/main/java/tech/team1781/subsystems/Arm.java
@@ -126,10 +126,10 @@ public class Arm extends Subsystem {
                 break;
 
             case SUBWOOFER:
-                break;
+                return matchesPosition();
 
             case COLLECT:
-                break;
+                return matchesPosition();
         }
         return false;
     }
@@ -168,5 +168,10 @@ public class Arm extends Subsystem {
 
     public double getAngle() {
         return mLeftEncoder.getPosition();
+    }
+
+    private boolean matchesPosition() {
+        var diff = mDesiredPosition - mLeftEncoder.getPosition();
+        return Math.abs(diff) <= 20;
     }
 }
