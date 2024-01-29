40adda1db320f9a50c9e34c27a5b2b8b9dba7830
Joaquin Herrera <jherrera132@cps.edu>
2024-01-29 17:18:45 -0600

    Added soft limit, burned flash, building new funcs
    Added and burned soft limit + current limit. Working on functions to calculate optimal angle for shooting/placement.

--- roborio/crescendo/src/main/java/tech/team1781/subsystems/Arm.java
+++ roborio/crescendo/src/main/java/tech/team1781/subsystems/Arm.java
@@ -19,6 +19,8 @@ public class Arm extends Subsystem {
     private CANSparkMax mRightMotor;
     private CANSparkMax mLeftMotor;
     private RelativeEncoder mLeftEncoder;
+    private SparkLimitSwitch mDownLimitSwitch;
+    private SparkLimitSwitch mUpLimitSwitch;
     private ArmState mDesiredArmState;
     private ProfiledPIDController mPositionPID = new ProfiledPIDController(0.1, 0, 0,
             new TrapezoidProfile.Constraints(200, 50));
@@ -43,6 +45,11 @@ public class Arm extends Subsystem {
         mLeftEncoder = mLeftMotor.getEncoder();
         mLeftEncoder.setPositionConversionFactor(ConfigMap.ARM_GEAR_RATIO * 360.0 * 1.2); // will tell us angle in
                                                                                           // degrees
+        
+        mDownLimitSwitch = mLeftMotor.getReverseLimitSwitch(Type.kNormallyOpen);
+        mUpLimitSwitch = mLeftMotor.getForwardLimitSwitch(Type.kNormallyOpen);
+
+
         mDesiredArmState = ArmState.START;
 
         mRightMotor.follow(mLeftMotor, true);
@@ -66,7 +73,8 @@ public class Arm extends Subsystem {
         SAFE,
         PODIUM,
         SUBWOOFER,
-        COLLECT
+        COLLECT,
+        AUTO_AIM
     }
 
     @Override
@@ -75,6 +83,10 @@ public class Arm extends Subsystem {
 
     @Override
     public void init() {
+        mLeftMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+        mLeftMotor.setSmartCurrentLimit(30);
+        mLeftMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 90);
+        mLeftMotor.burnFlash();
         if (currentMode == OperatingMode.DISABLED) {
         }
     }
@@ -151,19 +163,42 @@ public class Arm extends Subsystem {
         }
     }
 
+    private boolean overExtended() {
+        if (mUpLimitSwitch.isPressed()) {
+            System.out.println("Arm limit switch hit");
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     @Override
     public void teleopPeriodic() {
 
         // System.out.printf("arm left encoder %.3f\n", getAngle());
         // mLeftMotor.set(0); //temp
     }
+    
 
     @Override
     public void setDesiredState(SubsystemState state) {
         super.setDesiredState(state);
-
         mIsManual = false;
-        mDesiredPosition = mPositions.get(state);
+        if (mDesiredArmState != ArmState.AUTO_AIM) {
+            mDesiredPosition = mPositions.get(state);
+        }
+
+    }
+
+    public void setOptimalAngle(double distance) {
+        // Calculate desired angle
+        // Find desired angle based on distance
+        mDesiredPosition = 25.0;
+    }
+
+    public boolean atOptimalAngle() {
+        return Math.abs(getAngle() - mDesiredPosition) < ConfigMap.ARM_POSITION_TOLERANCE;
     }
 
     public double getAngle() {
