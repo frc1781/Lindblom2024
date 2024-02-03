c401a610c3efef0a89e7464ea7ac41974ab6f80a
asingharoy <asingharoy@cps.edu>
2024-02-03 12:29:31 -0600

    created the herding path

--- roborio/crescendo/src/main/java/tech/team1781/Paths.java
+++ roborio/crescendo/src/main/java/tech/team1781/Paths.java
@@ -8,4 +8,5 @@ public class Paths {
     public static final PathPlannerPath p2r3Path = PathPlannerPath.fromPathFile("p2;r3");
     public static final PathPlannerPath r3r2Path = PathPlannerPath.fromPathFile("r3;r2");
     public static final PathPlannerPath r2r1Path = PathPlannerPath.fromPathFile("r2;r1");
+    public static final PathPlannerPath HockeyStratPath = PathPlannerPath.fromPathFile(("HockeyStrat"));
 }
