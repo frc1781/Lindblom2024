045a7b4dbf1f4ecaa4e05f6b265309c55d6a0762
Vincent Dizon <vdizon@cps.edu>
2024-02-19 08:22:55 -0600

    new routine

---
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/SYSIDRoutine.java
@@ -0,0 +1,17 @@
+package tech.team1781.autonomous.routines;
+
+import tech.team1781.autonomous.AutoStep;
+import tech.team1781.autonomous.AutonomousHandler.AutoRoutine;
+
+public class SYSIDRoutine implements AutoRoutine{
+
+    @Override
+    public String getName() {
+        return "System Identification";
+    }
+
+    @Override
+    public AutoStep[] getSteps() {
+    }
+    
+}
