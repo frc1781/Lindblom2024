efb30c175c706ce6ba064e258e7bb40d72903509
Keith Van Rhein <kvanrhein@cps.edu>
2024-02-28 10:41:55 -0600

    asdf

--- roborio/crescendo/src/main/java/tech/team1781/subsystems/DriveSystem.java
+++ roborio/crescendo/src/main/java/tech/team1781/subsystems/DriveSystem.java
@@ -82,15 +82,20 @@ public class DriveSystem extends Subsystem {
     private HolonomicDriveController mTrajectoryController = new HolonomicDriveController(mXController, mYController,
             mRotController);
 
-    private GenericEntry mRobotXEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "X Position", -1, ShuffleboardStyle.ROBOT_X);
-    private GenericEntry mRobotYEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Y Position", -1, ShuffleboardStyle.ROBOT_Y);
-    private GenericEntry mRobotThetaEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Theta", -1, ShuffleboardStyle.ROBOT_THETA);
-    private GenericEntry mRobotXSpeedEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "X Speed", -1, ShuffleboardStyle.ROBOT_X_VELOCITY);
-    private GenericEntry mRobotYSpeedEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Y Speed", -1, ShuffleboardStyle.ROBOT_Y_VELOCITY);
-    private GenericEntry mRobotVelocityEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Overall Velocity", -1, ShuffleboardStyle.ROBOT_VELOCITY);
-    
-    private Field2d mField = new Field2d();
+    private GenericEntry mRobotXEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "X Position", -1,
+            ShuffleboardStyle.ROBOT_X);
+    private GenericEntry mRobotYEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Y Position", -1,
+            ShuffleboardStyle.ROBOT_Y);
+    private GenericEntry mRobotThetaEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Theta", -1,
+            ShuffleboardStyle.ROBOT_THETA);
+    private GenericEntry mRobotXSpeedEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "X Speed", -1,
+            ShuffleboardStyle.ROBOT_X_VELOCITY);
+    private GenericEntry mRobotYSpeedEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB, "Y Speed", -1,
+            ShuffleboardStyle.ROBOT_Y_VELOCITY);
+    private GenericEntry mRobotVelocityEntry = ShuffleboardStyle.getEntry(ConfigMap.SHUFFLEBOARD_TAB,
+            "Overall Velocity", -1, ShuffleboardStyle.ROBOT_VELOCITY);
 
+    private Field2d mField = new Field2d();
 
     public DriveSystem() {
         super("Drive System", DriveSystemState.DRIVE_MANUAL);
@@ -102,8 +107,10 @@ public class DriveSystem extends Subsystem {
         mNavX.resetDisplacement();
 
         ConfigMap.SHUFFLEBOARD_TAB.add("Field", mField)
-        .withPosition((int)ShuffleboardStyle.ROBOT_POSITION_FIELD.position.x,(int) ShuffleboardStyle.ROBOT_POSITION_FIELD.position.y)
-        .withSize((int) ShuffleboardStyle.ROBOT_POSITION_FIELD.size.x, (int) ShuffleboardStyle.ROBOT_POSITION_FIELD.size.y);
+                .withPosition((int) ShuffleboardStyle.ROBOT_POSITION_FIELD.position.x,
+                        (int) ShuffleboardStyle.ROBOT_POSITION_FIELD.position.y)
+                .withSize((int) ShuffleboardStyle.ROBOT_POSITION_FIELD.size.x,
+                        (int) ShuffleboardStyle.ROBOT_POSITION_FIELD.size.y);
     }
 
     public enum DriveSystemState implements Subsystem.SubsystemState {
@@ -121,7 +128,8 @@ public class DriveSystem extends Subsystem {
                 break;
             case DRIVE_TRAJECTORY:
                 var trajectoryInitialPose = mDesiredTrajectory.getInitialState().getTargetHolonomicPose();
-                System.out.println(mDesiredTrajectory.hashCode() + " :: " + EVector.fromPose(trajectoryInitialPose));
+                // System.out.println(mDesiredTrajectory.hashCode() + " :: " +
+                // EVector.fromPose(trajectoryInitialPose));
                 followTrajectory();
                 break;
             case DRIVE_MANUAL:
@@ -132,8 +140,9 @@ public class DriveSystem extends Subsystem {
             case SYSID:
                 driveRaw(1, 0, 0);
                 ChassisSpeeds currentSpeeds = getChassisSpeeds();
-                System.out.print(super.currentTime + "," + currentSpeeds.vxMetersPerSecond + "," + currentSpeeds.vyMetersPerSecond + "," + currentSpeeds.omegaRadiansPerSecond);
-            break;
+                System.out.print(super.currentTime + "," + currentSpeeds.vxMetersPerSecond + ","
+                        + currentSpeeds.vyMetersPerSecond + "," + currentSpeeds.omegaRadiansPerSecond);
+                break;
             default:
                 break;
         }
@@ -174,8 +183,8 @@ public class DriveSystem extends Subsystem {
         mRobotXSpeedEntry.setDouble(Math.abs(getChassisSpeeds().vxMetersPerSecond));
         mRobotYSpeedEntry.setDouble(Math.abs(getChassisSpeeds().vyMetersPerSecond));
         mRobotVelocityEntry.setDouble(
-            EVector.newVector(getChassisSpeeds().vxMetersPerSecond, getChassisSpeeds().vyMetersPerSecond).magnitude()
-        );
+                EVector.newVector(getChassisSpeeds().vxMetersPerSecond, getChassisSpeeds().vyMetersPerSecond)
+                        .magnitude());
         mRobotThetaEntry.setDouble(getRobotAngle().getRadians());
         mField.setRobotPose(getRobotPose());
         // mField.setRobotPose(getRobotPose());
@@ -188,21 +197,19 @@ public class DriveSystem extends Subsystem {
         mFrontRight.init();
         mBackLeft.init();
         mBackRight.init();
-        mNavX.reset();
-        mNavX.zeroYaw();
 
         double startingAngle = DriverStation.getAlliance().get() == Alliance.Red ? Math.PI : 0;
 
         switch (currentMode) {
             case AUTONOMOUS:
+                mNavX.reset();
+                mNavX.zeroYaw();
                 mIsFieldOriented = true;
                 mHasNavXOffsetBeenSet = false;
                 mOdometryBeenSet = false;
                 break;
             case TELEOP:
-                // setOdometry(new Pose2d(1.26, 5.53, new Rotation2d()));
                 mHasNavXOffsetBeenSet = false;
-                setNavXOffset(new Rotation2d(startingAngle));
                 mIsFieldOriented = true;
                 mIsManual = true;
                 break;
@@ -217,15 +224,16 @@ public class DriveSystem extends Subsystem {
         }
 
         // Runnable OdometryLogging = () -> {
-        //     Pose2d robotPose = getRobotPose();
-        //     super.mNetworkLogger.log("X", robotPose.getX());
-        //     super.mNetworkLogger.log("Y", robotPose.getY());
-        //     super.mNetworkLogger.log("Rot", getRobotAngle().getRadians());
+        // Pose2d robotPose = getRobotPose();
+        // super.mNetworkLogger.log("X", robotPose.getX());
+        // super.mNetworkLogger.log("Y", robotPose.getY());
+        // super.mNetworkLogger.log("Rot", getRobotAngle().getRadians());
         // };
 
         mRotController.enableContinuousInput(0, 2 * Math.PI);
 
-        // ScheduledExecutorService loggingExecutor = Executors.newScheduledThreadPool(1);
+        // ScheduledExecutorService loggingExecutor =
+        // Executors.newScheduledThreadPool(1);
         // loggingExecutor.scheduleAtFixedRate(OdometryLogging, 0, 1, TimeUnit.SECONDS);
     }
 
@@ -235,7 +243,7 @@ public class DriveSystem extends Subsystem {
 
         visionEstimateVector.z = currentPose.z;
 
-        if(Math.abs(currentPose.dist(visionEstimateVector)) >= 1) {
+        if (Math.abs(currentPose.dist(visionEstimateVector)) >= 1) {
             return;
         }
 
@@ -253,7 +261,10 @@ public class DriveSystem extends Subsystem {
         }
 
         mNavXOffset = offset.getRadians();
-        // System.out.printf("navx offset set to %.2f\n", mNavXOffset);
+        mPoseEstimator = new SwerveDrivePoseEstimator(mKinematics, getRobotAngle(), getModulePositions(),
+                getRobotPose());
+        // mPoseEstimator.resetPosition(getRobotAngle(), getModulePositions(), )
+        System.out.printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>navx offset set to %.2f\n", mNavXOffset);
         mHasNavXOffsetBeenSet = true;
     }
 
@@ -278,6 +289,11 @@ public class DriveSystem extends Subsystem {
         // pathplannerState.getTargetHolonomicPose(),
         // pathplannerState.curvatureRadPerMeter);
         // System.out.println(pathplannerState.velocityMps);
+        System.out.printf("ra: %.2f hpr: %.2f ppsh: %.2f\n",
+                getRobotPose().getRotation().getDegrees(),
+                pathplannerState.getTargetHolonomicPose().getRotation().getDegrees(),
+                pathplannerState.heading.getDegrees());
+
         ChassisSpeeds desiredChassisSpeeds = mTrajectoryController.calculate(
                 getRobotPose(),
                 new Pose2d(pathplannerState.positionMeters, pathplannerState.heading),
@@ -288,17 +304,17 @@ public class DriveSystem extends Subsystem {
 
     public void aimSpeaker(boolean isAiming) {
         mIsAiming = isAiming;
-        if(!isAiming) {
+        if (!isAiming) {
             return;
         }
-        
+
         EVector speakerpos = ControlSystem.isRed() ? ConfigMap.RED_SPEAKER_LOCATION : ConfigMap.BLUE_SPEAKER_LOCATION;
         EVector currentPose = EVector.fromPose(getRobotPose());
         currentPose.z = 0;
 
         double angle = currentPose.angleBetween(speakerpos) - Math.PI;
         angle = normalizeRadians(angle);
-        
+
         mDesiredAngle = angle;
     }
 
@@ -325,7 +341,6 @@ public class DriveSystem extends Subsystem {
         mDesiredTrajectory = trajectory;
 
         if (!mOdometryBeenSet) {
-            setNavXOffset(new Rotation2d((45.0 / 180.0 * Math.PI)));
             setOdometry(initialPose);
             mOdometryBeenSet = true;
         }
@@ -334,7 +349,13 @@ public class DriveSystem extends Subsystem {
     }
 
     public void setTrajectoryFromPath(PathPlannerPath path) {
-        setNavXOffset(path.getPreviewStartingHolonomicPose().getRotation());
+        Rotation2d startingOrientation = path.getPreviewStartingHolonomicPose().getRotation();
+        setNavXOffset(startingOrientation);
+        System.out.printf(">>>>path making>>> staring Orientation: %.2f,%.2f,%.2f,%.2f\n",
+                startingOrientation.getDegrees(),
+                getRobotAngle().getDegrees(),
+                mNavXOffset,
+                getRobotPose().getRotation().getDegrees());
         // also use current speed of robot
         PathPlannerTrajectory pathTrajectory = new PathPlannerTrajectory(path, new ChassisSpeeds(), getRobotAngle());
         setTrajectory(pathTrajectory);
@@ -369,10 +390,11 @@ public class DriveSystem extends Subsystem {
         SwerveModuleState[] moduleStates = mKinematics.toSwerveModuleStates(
                 mIsFieldOriented
                         ? ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(
-                            xSpeed, 
-                            ySpeed, 
-                            mIsAiming ? mRotController.calculate(getRobotAngle().getRadians(), mDesiredAngle) : rot), 
-                            getRobotAngle())
+                                xSpeed,
+                                ySpeed,
+                                mIsAiming ? mRotController.calculate(getRobotAngle().getRadians(), mDesiredAngle)
+                                        : rot),
+                                getRobotAngle())
                         : new ChassisSpeeds(xSpeed, ySpeed, rot));
 
         SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, ConfigMap.MAX_VELOCITY_METERS_PER_SECOND);
@@ -424,6 +446,10 @@ public class DriveSystem extends Subsystem {
 
     private void updateOdometry() {
         // mOdometry.update(getRobotAngle(), getModulePositions());
+        // System.out.printf("%.2f,%.2f,%.2f\n",
+        // getRobotAngle().getDegrees(),
+        // mNavXOffset,
+        // getRobotPose().getRotation().getDegrees());
         mPoseEstimator.update(getRobotAngle(), getModulePositions());
     }
 
@@ -434,11 +460,11 @@ public class DriveSystem extends Subsystem {
         // ((NEOL1SwerveModule) mBackRight).printModuleState();
     }
 
-    private double normalizeRadians(double rads){
+    private double normalizeRadians(double rads) {
         rads %= 2 * Math.PI;
 
-        if(rads < 0) {
-            rads += 2 * Math.PI; 
+        if (rads < 0) {
+            rads += 2 * Math.PI;
         }
 
         return rads;
