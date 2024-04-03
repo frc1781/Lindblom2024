5a49cc14c3252f91fa63eddc86871cfc9a707a4e
Keith Van Rhein <kvanrhein@cps.edu>
2024-04-03 15:51:53 -0500

    Update TestRoutine.java

--- roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/TestRoutine.java
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/TestRoutine.java
@@ -1,5 +1,6 @@
 package tech.team1781.autonomous.routines;
 
+import tech.team1781.Paths;
 import tech.team1781.autonomous.AutoStep;
 import tech.team1781.autonomous.AutonomousHandler.AutoRoutine;
 import tech.team1781.control.ControlSystem;
@@ -18,8 +19,7 @@ public class TestRoutine implements AutoRoutine {
     public AutoStep[] getSteps() {
         return new AutoStep[] {
             //      new AutoStep(100,Action.OFF_KICKSTAND),
-          new AutoStep(0.1, Positions.P2),
-          new AutoStep(2, Positions.N2),
+          new AutoStep(3.0, Paths.getPathFromName("p2;n2"))
         };
     }
 
