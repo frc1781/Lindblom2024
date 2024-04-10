5fa9fc2d3f793d36747bc7188100eeb46ef187b0
Vincent Dizon <vdizon@cps.edu>
2024-04-10 16:25:00 -0500

    .

--- roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/TestRoutine.java
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/TestRoutine.java
@@ -17,7 +17,7 @@ public class TestRoutine implements AutoRoutine {
     @Override
     public AutoStep[] getSteps() {
         return new AutoStep[] {
-            new AutoStep(5,Paths.getPathFromName("p2;c5")), 
+            new AutoStep(5,Paths.getPathFromName("Test Path")), 
         };
     }
 
