dc69a75c6420698cdef6f37479bef3faf39e7e1d
Jaylon spears <Jkspears2@cps.edu>
2024-09-24 11:23:26 -0500

    changed a few routines to paths

--- roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P1N1Subwoofer.java
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P1N1Subwoofer.java
@@ -1,5 +1,7 @@
 package tech.team1781.autonomous.routines;
 
+
+import tech.team1781.Paths;
 import tech.team1781.autonomous.AutoStep;
 import tech.team1781.autonomous.Positions;
 import tech.team1781.autonomous.AutonomousHandler.AutoRoutine;
@@ -16,15 +18,9 @@ public class P1N1Subwoofer implements AutoRoutine {
     @Override
     public AutoStep[] getSteps() {
         return new AutoStep[] {
-                new AutoStep(100, Action.OFF_KICKSTAND),
-                new AutoStep(0.1, Positions.P1),
                 new AutoStep(5, Action.SHOOT_SUBWOOFER_NO_AIM),
-                new AutoStep(2, Action.COLLECT_RAMP, EVector.positionWithDegrees(1.88, 6.98, 0)),
-                new AutoStep(2, Action.COLLECT_RAMP,Positions.N1, true), 
-                new AutoStep(3, Action.COLLECT_RAMP, Positions.P1),
+                new AutoStep(5, Paths.getPathFromName("p1;n1")),
                 new AutoStep(2, Action.AUTO_AIM_SHOOT),
-                
-
         };
     }
 
