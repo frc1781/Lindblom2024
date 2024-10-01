6e5a0217adf8ae1258ac64ad684ab1814864bfbd
asingharoy <asingharoy@cps.edu>
2024-10-01 11:22:38 -0500

    changed more routines (towards the p1 side) to paths

--- roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P1N1C1Subwoofer.java
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P1N1C1Subwoofer.java
@@ -1,5 +1,6 @@
 package tech.team1781.autonomous.routines;
 
+import tech.team1781.Paths;
 import tech.team1781.autonomous.AutoStep;
 import tech.team1781.autonomous.Positions;
 import tech.team1781.autonomous.AutonomousHandler.AutoRoutine;
@@ -16,16 +17,11 @@ public class P1N1C1Subwoofer implements AutoRoutine {
     @Override
     public AutoStep[] getSteps() {
         return new AutoStep[] {
-                new AutoStep(100, Action.OFF_KICKSTAND),
-                new AutoStep(0.1, Positions.P1),
                 new AutoStep(5, Action.SHOOT_SUBWOOFER_NO_AIM),
-                new AutoStep(2, Action.COLLECT_RAMP),
-                new AutoStep(2, Action.COLLECT_RAMP, Positions.N1, true),
-                new AutoStep(3, Action.COLLECT_RAMP_STAY_DOWN, Positions.P1),
+                new AutoStep(5, Action.COLLECT_RAMP, Paths.getPathFromName("p1;n1")),
                 new AutoStep(5, Action.SHOOT_SUBWOOFER_NO_AIM),
-                new AutoStep(10, Action.COLLECT_RAMP_STAY_DOWN, Positions.C1, true),
-                new AutoStep(1.5, Action.COLLECT_RAMP_STAY_DOWN, EVector.positionWithDegrees(5, 6.8, 0)),
-                new AutoStep(2.5, Action.COLLECT_RAMP, EVector.positionWithDegrees(4.25, 5.5, 0)),
+                new AutoStep(5, Action.COLLECT_RAMP, Paths.getPathFromName("p1;n1")),
+                new AutoStep(5, Action.COLLECT_RAMP, Paths.getPathFromName("c1;shootAmpSide")),
                 new AutoStep(2.5, Action.SHOOT_FAR),
         };
     }
