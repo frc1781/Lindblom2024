039a861a56edeeb46692cc3175b1c0013cb653d1
Ally <67673392+Allybe@users.noreply.github.com>
2024-10-11 12:48:30 -0500

    simpler paths for harder notes

--- roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P2FourNote.java
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P2FourNote.java
@@ -3,6 +3,7 @@ package tech.team1781.autonomous.routines;
 import tech.team1781.Paths;
 import tech.team1781.autonomous.AutoStep;
 import tech.team1781.autonomous.AutonomousHandler.AutoRoutine;
+import tech.team1781.control.ControlSystem;
 import tech.team1781.control.ControlSystem.Action;
 
 public class P2FourNote implements AutoRoutine {
@@ -21,9 +22,9 @@ public class P2FourNote implements AutoRoutine {
                 new AutoStep(5, Action.COLLECT_RAMP_STAY_DOWN, Paths.getPathFromName("n2;n3"), true, true),
                 new AutoStep(5, Action.COLLECT_RAMP_STAY_DOWN, Paths.getPathFromName("n3;p2")),
                 new AutoStep(5, Action.SHOOT_SUBWOOFER_NO_AIM),
-                new AutoStep(5, Action.COLLECT_RAMP, Paths.getPathFromName("p2;n1"), true, true),
+                new AutoStep(5, Action.COLLECT_RAMP, Paths.getPathFromName("p2;n1;simple"), true, true),
+                new AutoStep(5, Action.RAMP_SHOOTER, Paths.getPathFromName("n1;p2;simple")),
                 new AutoStep(5, Action.SHOOT_NOTE_ONE)
         };
     }
-
 }
