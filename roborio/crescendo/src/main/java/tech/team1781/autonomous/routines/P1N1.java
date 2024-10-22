358d83311115d0385bc783bbc29dc7dec0ba1cd0
Allison <jamiranda4@cps.edu>
2024-10-22 11:20:35 -0500

    just one

--- roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P1N1.java
+++ roborio/crescendo/src/main/java/tech/team1781/autonomous/routines/P1N1.java
@@ -15,6 +15,7 @@ public class P1N1 implements AutoRoutine {
     @Override
     public AutoStep[] getSteps() {
         return new AutoStep[] {
+            new AutoStep(100, Action.OFF_KICKSTAND),
             new AutoStep(6, Action.SHOOT_SUBWOOFER_NO_AIM),
             new AutoStep(5, Action.COLLECT_RAMP, Paths.getPathFromName("p1;n1")),
             new AutoStep(5, Action.RAMP_SHOOTER, Paths.getPathFromName("n1;p1")),
